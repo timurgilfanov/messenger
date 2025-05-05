e21f254741ef875679a24ac1fa5e94a2dffc40e4
Timur Gilfanov <timurgilfanov@gmail.com>
2025-05-05 19:14:06 +0400

    Enable Android test coverage for debug builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            enableAndroidTestCoverage = project.hasProperty("coverage")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(

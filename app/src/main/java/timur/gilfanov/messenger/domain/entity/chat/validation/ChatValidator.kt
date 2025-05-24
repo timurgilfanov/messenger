dc32abbb97871b210d3605b674c4711138336c52
Timur Gilfanov <timurgilfanov@gmail.com>
2025-05-24 21:55:39 +0400

    Fail build CI step if detekt found errors (#8)
    * Fail build CI step if detekt found errors
    * Configure LongMethod rule to ignore annotated Test functions

--- app/src/main/java/timur/gilfanov/messenger/domain/entity/chat/validation/ChatValidator.kt
+++ app/src/main/java/timur/gilfanov/messenger/domain/entity/chat/validation/ChatValidator.kt
@@ -17,28 +17,24 @@ import timur.gilfanov.messenger.domain.entity.message.MessageId
 class ChatValidator {
     fun validate(chat: Chat): ResultWithError<Unit, ChatValidationError> {
         val participantsResult = validateParticipants(chat.participants)
-        if (participantsResult.isFailure) return participantsResult
-
-        val nameResult = validateName(chat.name)
-        if (nameResult.isFailure) return nameResult
-
-        return ResultWithError.Success(Unit)
+        if (participantsResult.isFailure) {
+            return participantsResult
+        } else {
+            val nameResult = validateName(chat.name)
+            return if (nameResult.isFailure) nameResult else ResultWithError.Success(Unit)
+        }
     }
 
-    fun validateOnCreation(chat: Chat): ResultWithError<Unit, ChatValidationError> {
-        val validationResult = validate(chat)
-        if (validationResult.isFailure) return validationResult
-
-        val messagesResult = validateMessagesOnCreation(chat.messages)
-        if (messagesResult.isFailure) return messagesResult
-
-        val unreadCountResult = validateUnreadMessagesCountOnCreation(chat.unreadMessagesCount)
-        if (unreadCountResult.isFailure) return unreadCountResult
-
-        val lastReadMessageResult = validateLastReadMessageIdOnCreation(chat.lastReadMessageId)
-        if (lastReadMessageResult.isFailure) return lastReadMessageResult
-
-        return ResultWithError.Success(Unit)
+    fun validateOnCreation(chat: Chat): ResultWithError<Unit, ChatValidationError> = when {
+        validate(chat).isFailure -> validate(chat)
+        validateMessagesOnCreation(
+            chat.messages,
+        ).isFailure -> validateMessagesOnCreation(chat.messages)
+        validateUnreadMessagesCountOnCreation(chat.unreadMessagesCount).isFailure ->
+            validateUnreadMessagesCountOnCreation(chat.unreadMessagesCount)
+        validateLastReadMessageIdOnCreation(chat.lastReadMessageId).isFailure ->
+            validateLastReadMessageIdOnCreation(chat.lastReadMessageId)
+        else -> ResultWithError.Success(Unit)
     }
 
     fun validateParticipants(

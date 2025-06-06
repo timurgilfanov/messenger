17fbfe27cb6ec8cd951c81d4b488c8a416a37d0a
Timur Gilfanov <timurgilfanov@gmail.com>
2025-06-06 21:47:19 +0400

    Add error handling to flowChatList

--- app/src/main/java/timur/gilfanov/messenger/domain/usecase/participant/ParticipantRepository.kt
+++ app/src/main/java/timur/gilfanov/messenger/domain/usecase/participant/ParticipantRepository.kt
@@ -6,6 +6,7 @@ import timur.gilfanov.messenger.domain.entity.chat.Chat
 import timur.gilfanov.messenger.domain.entity.chat.ChatId
 import timur.gilfanov.messenger.domain.entity.message.Message
 import timur.gilfanov.messenger.domain.entity.message.MessageId
+import timur.gilfanov.messenger.domain.usecase.participant.chat.FlowChatListError
 import timur.gilfanov.messenger.domain.usecase.participant.chat.ReceiveChatUpdatesError
 import timur.gilfanov.messenger.domain.usecase.participant.chat.RepositoryJoinChatError
 import timur.gilfanov.messenger.domain.usecase.participant.chat.RepositoryLeaveChatError
@@ -15,7 +16,7 @@ import timur.gilfanov.messenger.domain.usecase.participant.message.RepositoryDel
 interface ParticipantRepository {
     suspend fun sendMessage(message: Message): Flow<Message>
     suspend fun editMessage(message: Message): Flow<Message>
-    suspend fun flowChatList(): Flow<List<Chat>>
+    suspend fun flowChatList(): Flow<ResultWithError<List<Chat>, FlowChatListError>>
     suspend fun deleteMessage(
         messageId: MessageId,
         mode: DeleteMessageMode,
--- app/src/test/java/timur/gilfanov/messenger/domain/usecase/participant/chat/FlowChatListUseCaseTest.kt
+++ app/src/test/java/timur/gilfanov/messenger/domain/usecase/participant/chat/FlowChatListUseCaseTest.kt
@@ -1,22 +1,26 @@
 package timur.gilfanov.messenger.domain.usecase.participant.chat
 
 import app.cash.turbine.test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
-import kotlin.test.assertEquals
+import timur.gilfanov.messenger.domain.entity.ResultWithError
 import timur.gilfanov.messenger.domain.entity.chat.Chat
 import timur.gilfanov.messenger.domain.entity.chat.buildChat
 import timur.gilfanov.messenger.domain.usecase.participant.ParticipantRepository
 import timur.gilfanov.messenger.domain.usecase.participant.ParticipantRepositoryNotImplemented
+import timur.gilfanov.messenger.domain.usecase.participant.chat.FlowChatListError
 
 class FlowChatListUseCaseTest {
 
     private class RepositoryFake(
-        val chatListFlow: Flow<List<Chat>>,
+        val chatListFlow: Flow<ResultWithError<List<Chat>, FlowChatListError>>,
     ) : ParticipantRepository by ParticipantRepositoryNotImplemented() {
-        override suspend fun flowChatList(): Flow<List<Chat>> = chatListFlow
+        override suspend fun flowChatList(): Flow<ResultWithError<List<Chat>, FlowChatListError>> =
+            chatListFlow
     }
 
     @Test
@@ -33,8 +37,13 @@ class FlowChatListUseCaseTest {
         val useCase = FlowChatListUseCase(repository)
 
         useCase().test {
-            assertEquals(listOf(chat1), awaitItem())
-            assertEquals(listOf(chat1, chat2), awaitItem())
+            val first = awaitItem()
+            assertIs<ResultWithError.Success<List<Chat>, FlowChatListError>>(first)
+            assertEquals(listOf(chat1), first.data)
+
+            val second = awaitItem()
+            assertIs<ResultWithError.Success<List<Chat>, FlowChatListError>>(second)
+            assertEquals(listOf(chat1, chat2), second.data)
             awaitComplete()
         }
     }

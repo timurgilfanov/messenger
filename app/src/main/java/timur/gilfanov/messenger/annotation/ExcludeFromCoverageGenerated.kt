0ae53aebb23ac4da44b3696d03451dc98ed8aba5
Timur Gilfanov <timurgilfanov@gmail.com>
2025-07-10 01:19:32 +0400

    Change retention policy of ExcludeFromCoverageGenerated annotation to RUNTIME

--- app/src/main/java/timur/gilfanov/messenger/annotation/ExcludeFromCoverageGenerated.kt
+++ app/src/main/java/timur/gilfanov/messenger/annotation/ExcludeFromCoverageGenerated.kt
@@ -1,5 +1,5 @@
 package timur.gilfanov.messenger.annotation
 
-@Retention(AnnotationRetention.BINARY)
+@Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
 annotation class ExcludeFromCoverageGenerated

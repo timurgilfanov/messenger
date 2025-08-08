5ce91fb061fbdf119ced40805c00228901b92d83
Timur Gilfanov <timurgilfanov@gmail.com>
2025-08-08 14:19:10 +0400

    Remove SyncMetadata from database and Dao, we will use simpler data structure and storage to store sync data.

--- app/src/main/java/timur/gilfanov/messenger/data/source/local/database/MessengerDatabase.kt
+++ app/src/main/java/timur/gilfanov/messenger/data/source/local/database/MessengerDatabase.kt
@@ -7,16 +7,14 @@ import timur.gilfanov.messenger.data.source.local.database.converter.Converters
 import timur.gilfanov.messenger.data.source.local.database.dao.ChatDao
 import timur.gilfanov.messenger.data.source.local.database.dao.MessageDao
 import timur.gilfanov.messenger.data.source.local.database.dao.ParticipantDao
-import timur.gilfanov.messenger.data.source.local.database.dao.SyncMetadataDao
 import timur.gilfanov.messenger.data.source.local.database.entity.ChatEntity
 import timur.gilfanov.messenger.data.source.local.database.entity.ChatParticipantCrossRef
 import timur.gilfanov.messenger.data.source.local.database.entity.MessageEntity
 import timur.gilfanov.messenger.data.source.local.database.entity.ParticipantEntity
-import timur.gilfanov.messenger.data.source.local.database.entity.SyncMetadataEntity
 
 /**
  * Room database for the Messenger application.
- * Manages all local data storage for chats, messages, participants, and sync metadata.
+ * Manages all local data storage for chats, messages, and participants.
  */
 @Database(
     entities = [
@@ -24,7 +22,6 @@ import timur.gilfanov.messenger.data.source.local.database.entity.SyncMetadataEn
         MessageEntity::class,
         ParticipantEntity::class,
         ChatParticipantCrossRef::class,
-        SyncMetadataEntity::class,
     ],
     version = 1,
     exportSchema = true,
@@ -34,7 +31,6 @@ abstract class MessengerDatabase : RoomDatabase() {
     abstract fun chatDao(): ChatDao
     abstract fun messageDao(): MessageDao
     abstract fun participantDao(): ParticipantDao
-    abstract fun syncMetadataDao(): SyncMetadataDao
 
     companion object {
         const val DATABASE_NAME = "messenger_database"
--- app/src/main/java/timur/gilfanov/messenger/data/source/local/database/converter/Converters.kt
+++ app/src/main/java/timur/gilfanov/messenger/data/source/local/database/converter/Converters.kt
@@ -3,7 +3,6 @@ package timur.gilfanov.messenger.data.source.local.database.converter
 import androidx.room.TypeConverter
 import kotlinx.datetime.Instant
 import timur.gilfanov.messenger.data.source.local.database.entity.MessageType
-import timur.gilfanov.messenger.data.source.local.database.entity.SyncStatus
 
 /**
  * Type converters for Room database.
@@ -22,10 +21,4 @@ class Converters {
 
     @TypeConverter
     fun toMessageType(type: String): MessageType = MessageType.valueOf(type)
-
-    @TypeConverter
-    fun fromSyncStatus(status: SyncStatus): String = status.name
-
-    @TypeConverter
-    fun toSyncStatus(status: String): SyncStatus = SyncStatus.valueOf(status)
 }

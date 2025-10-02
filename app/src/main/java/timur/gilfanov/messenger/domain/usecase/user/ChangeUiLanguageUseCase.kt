aac2c51fe44bed12c0e2972a743be553a30f0433
Timur Gilfanov <timurgilfanov@gmail.com>
2025-10-02 19:40:14 +0400

    Add userId parameter to ChangeUiLanguageUseCase
    The `invoke` method of `ChangeUiLanguageUseCase` now requires a `userId` in addition to the `newUiLanguage`.
    
    This change allows the use case to associate the language change with a specific user.

--- app/src/main/java/timur/gilfanov/messenger/domain/usecase/user/ChangeUiLanguageUseCase.kt
+++ app/src/main/java/timur/gilfanov/messenger/domain/usecase/user/ChangeUiLanguageUseCase.kt
@@ -2,9 +2,13 @@ package timur.gilfanov.messenger.domain.usecase.user
 
 import timur.gilfanov.messenger.domain.entity.ResultWithError
 import timur.gilfanov.messenger.domain.entity.user.UiLanguage
+import timur.gilfanov.messenger.domain.entity.user.UserId
 
 class ChangeUiLanguageUseCase {
-    operator fun invoke(newUiLanguage: UiLanguage): ResultWithError<Unit, ChangeUiLanguageError> {
+    operator fun invoke(
+        userId: UserId,
+        newUiLanguage: UiLanguage,
+    ): ResultWithError<Unit, ChangeUiLanguageError> {
         TODO("Not implemented yet")
     }
 }

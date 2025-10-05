1695eb07f5c081a5ec5d81a28a17863bc76474d5
Timur Gilfanov <timurgilfanov@gmail.com>
2025-10-05 18:33:51 +0400

    Refactor: Update ProfileEditUiState to use specific validation error
    - Change the type of `nameInputValidationError` in `ProfileEditUiState` from the generic `TextValidationError` to the more specific `UpdateNameError`.
    - This provides a more precise error type for name validation within the user profile editing screen.

--- app/src/main/java/timur/gilfanov/messenger/ui/screen/user/ProfileEditUiState.kt
+++ app/src/main/java/timur/gilfanov/messenger/ui/screen/user/ProfileEditUiState.kt
@@ -2,13 +2,13 @@ package timur.gilfanov.messenger.ui.screen.user
 
 import android.net.Uri
 import androidx.compose.foundation.text.input.TextFieldState
-import timur.gilfanov.messenger.domain.entity.message.validation.TextValidationError
 import timur.gilfanov.messenger.domain.usecase.user.RemovePictureError
+import timur.gilfanov.messenger.domain.usecase.user.UpdateNameError
 import timur.gilfanov.messenger.domain.usecase.user.UpdatePictureError
 
 data class ProfileEditUiState(
     val nameInput: TextFieldState,
-    val nameInputValidationError: TextValidationError? = null,
+    val nameInputValidationError: UpdateNameError? = null,
     val picture: Uri?,
     val pictureUpdatingError: UpdatePictureError? = null,
     val pictureUpdatingProgress: Int? = null,

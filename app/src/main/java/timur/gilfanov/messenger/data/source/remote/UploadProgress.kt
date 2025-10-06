dfb4696a8f5c7d9c0838a4cb811b156ca93ba09a
Timur Gilfanov <timurgilfanov@gmail.com>
2025-10-06 12:39:14 +0400

    Annotate Uploading percent with @IntRange
    - Add `@IntRange(from=0, to=100)` annotation to the `percent` parameter in the `Uploading` data class.
    - This provides compile-time checks and improves code clarity by indicating the expected range of values for the upload progress percentage.

--- app/src/main/java/timur/gilfanov/messenger/data/source/remote/UploadProgress.kt
+++ app/src/main/java/timur/gilfanov/messenger/data/source/remote/UploadProgress.kt
@@ -1,7 +1,9 @@
 package timur.gilfanov.messenger.data.source.remote
 
+import androidx.annotation.IntRange
+
 sealed interface UploadProgress {
-    data class Uploading(val percent: Int) : UploadProgress {
+    data class Uploading(@param:IntRange(from = 0, to = 100) val percent: Int) : UploadProgress {
         init {
             @Suppress("MagicNumber")
             require(percent in 0..100) { "Progress must be between 0 and 100" }

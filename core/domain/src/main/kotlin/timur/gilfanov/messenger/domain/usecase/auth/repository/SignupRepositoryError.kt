9f60e35ea49854860d9898cedac6679b7431e21b
Timur Gilfanov <timurgilfanov@gmail.com>
2026-03-14 17:29:04 +0400

    Add `UpdateProfileNameLogicalError` and refine `SignupRepositoryError`
    - Add `UpdateProfileNameLogicalError` interface with specific error cases: `LengthOutOfBounds`, `ForbiddenCharacter`, `PlatformPolicyViolation`, and `UnknownRuleViolation`
    - Update `SignupRepositoryError.InvalidName` to implement `UpdateProfileNameLogicalError`
    - Use `ImmutableList` for character lists in `ForbiddenCharacter` error case

--- core/domain/src/main/kotlin/timur/gilfanov/messenger/domain/usecase/auth/repository/SignupRepositoryError.kt
+++ core/domain/src/main/kotlin/timur/gilfanov/messenger/domain/usecase/auth/repository/SignupRepositoryError.kt
@@ -1,5 +1,6 @@
 package timur.gilfanov.messenger.domain.usecase.auth.repository
 
+import kotlinx.collections.immutable.ImmutableList
 import timur.gilfanov.messenger.domain.usecase.common.LocalStorageError
 import timur.gilfanov.messenger.domain.usecase.common.RemoteError
 
@@ -16,7 +17,41 @@ import timur.gilfanov.messenger.domain.usecase.common.RemoteError
  */
 sealed interface SignupRepositoryError {
     data object EmailTaken : SignupRepositoryError
-    data object InvalidName : SignupRepositoryError
+    data object InvalidName : SignupRepositoryError, UpdateProfileNameLogicalError
     data class LocalOperationFailed(val error: LocalStorageError) : SignupRepositoryError
     data class RemoteOperationFailed(val error: RemoteError) : SignupRepositoryError
+}
+
+sealed interface UpdateProfileNameLogicalError {
+    /**
+     * Name length does not meet requirements.
+     *
+     * @property length Actual length of the provided name
+     * @property min Minimum allowed length
+     * @property max Maximum allowed length
+     */
+    data class LengthOutOfBounds(val length: Int, val min: Int, val max: Int) :
+        UpdateProfileNameLogicalError
+
+    /**
+     * Name contains characters that are not allowed.
+     *
+     * @property usedForbiddenCharacters Forbidden characters found in the name
+     * @property forbiddenCharacters Complete list of forbidden characters
+     */
+    data class ForbiddenCharacter(
+        val usedForbiddenCharacters: ImmutableList<Char>,
+        val forbiddenCharacters: ImmutableList<Char>,
+    ) : UpdateProfileNameLogicalError
+
+    /**
+     * Name violates platform content policy.
+     */
+    sealed interface PlatformPolicyViolation : UpdateProfileNameLogicalError {
+        data object Pornography : PlatformPolicyViolation
+        data object Violence : PlatformPolicyViolation
+        data object IllegalSubstance : PlatformPolicyViolation
+    }
+
+    data class UnknownRuleViolation(val reason: String) : UpdateProfileNameLogicalError
 }

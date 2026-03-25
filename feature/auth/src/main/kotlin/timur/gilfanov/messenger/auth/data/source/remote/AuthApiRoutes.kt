605d2b864a32407486f166765ebfb11e343c6071
Timur Gilfanov <timurgilfanov@gmail.com>
2026-03-25 20:20:10 +0400

    refactor(auth): update Google authentication API routes

--- feature/auth/src/main/kotlin/timur/gilfanov/messenger/auth/data/source/remote/AuthApiRoutes.kt
+++ feature/auth/src/main/kotlin/timur/gilfanov/messenger/auth/data/source/remote/AuthApiRoutes.kt
@@ -2,9 +2,9 @@ package timur.gilfanov.messenger.auth.data.source.remote
 
 object AuthApiRoutes {
     const val LOGIN = "auth/login"
-    const val GOOGLE_LOGIN = "auth/google"
+    const val GOOGLE_LOGIN = "auth/login-google"
     const val REGISTER = "auth/register"
-    const val GOOGLE_REGISTER = "auth/google/register"
+    const val GOOGLE_REGISTER = "auth/register-google"
     const val REFRESH = "auth/refresh"
     const val LOGOUT = "auth/logout"
 }
